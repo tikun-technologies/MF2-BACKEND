101ab280d3876a5e878507b82f0ea7f30fef9721
dheeraj009joshi <73873399+dheeraj009joshi@users.noreply.github.com>
2025-02-17 15:58:03 +0530

    dj

--- server.py
+++ server.py
@@ -23,4 +23,4 @@ def index():
 
 if __name__ == "__main__":
     print("Server is running on http://127.0.0.1:5000")  # Debugging
-    app.run(host="0.0.0.0.",debug=True, port=5000)
+    app.run(host="0.0.0.0",debug=True, port=5000)
